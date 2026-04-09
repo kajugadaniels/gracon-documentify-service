bf9871353a1d0a3322de68a1049a8173b5033d27
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-09 21:20:56 +0200

    chore(documents): register PdfExportService in DocumentsModule

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
+import { PdfExportService } from './pdf-export.service';
 
 @Module({
   controllers: [DocumentsController],
-  providers: [DocumentsService],
+  providers: [DocumentsService, PdfExportService],
   exports: [DocumentsService],
 })
 export class DocumentsModule {}

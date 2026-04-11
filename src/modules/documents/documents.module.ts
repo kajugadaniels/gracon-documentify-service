55605798a901348958af5a1c1e490ae72c847954
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-11 11:52:21 +0200

    feat(documents): provide encryption service to documents module

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { EncryptionService } from '../../common/crypto/encryption.service';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
 import { PdfExportService } from './pdf-export.service';
 
 @Module({
   controllers: [DocumentsController],
-  providers: [DocumentsService, PdfExportService],
+  providers: [DocumentsService, PdfExportService, EncryptionService],
   exports: [DocumentsService],
 })
 export class DocumentsModule {}

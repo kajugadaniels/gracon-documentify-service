b1a9e2f0752200e5fd4f5f5a96ddb2f2a27afb41
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-04-13 20:48:59 +0200

    fix(documents): import AuthModule to resolve JwtService dependency in DocumentsModule

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EncryptionService } from '../../common/crypto/encryption.service';
+import { AuthModule } from '../auth/auth.module';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
 import { PdfExportService } from './pdf-export.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [DocumentsController],
   providers: [DocumentsService, PdfExportService, EncryptionService],
   exports: [DocumentsService],

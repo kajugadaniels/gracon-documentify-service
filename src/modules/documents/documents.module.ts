64890d7b0e1438c4547d701312d415a2cab3993f
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-05-16 19:22:28 +0200

    chore(documents): register document query service

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { EncryptionService } from '../../common/crypto/encryption.service';
 import { AuthModule } from '../auth/auth.module';
 import { DocumentsController } from './documents.controller';
+import { DocumentQueryService } from './document-query.service';
 import { DocumentsService } from './documents.service';
 import { PdfExportService } from './pdf-export.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [DocumentsController],
-  providers: [DocumentsService, PdfExportService, EncryptionService],
+  providers: [
+    DocumentsService,
+    DocumentQueryService,
+    PdfExportService,
+    EncryptionService,
+  ],
   exports: [DocumentsService],
 })
 export class DocumentsModule {}
